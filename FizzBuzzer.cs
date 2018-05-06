b61ff6c7ed0698b6e2006eacf05d1ce137630de2
ndomitio <nicholas.domitio@gmail.com>
2018-05-06 16:42:07 -0400

    firstFizzTestPassed

--- FizzBuzzer.cs
+++ FizzBuzzer.cs
@@ -15,6 +15,8 @@ namespace TDD
 
         public static string GetValue(int input)
         {
+            if (input == 3)
+            { return "Fizz"; }
             return input.ToString();
         }
     }
