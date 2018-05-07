1c158cb17affebfd49af1f936eda3f4bf772a993
ndomitio <nicholas.domitio@gmail.com>
2018-05-07 13:16:25 -0400

    FizzBuzzWorks!

--- FizzBuzzer.cs
+++ FizzBuzzer.cs
@@ -16,8 +16,16 @@ namespace TDD
         public static string GetValue(int input)
         {
 
-            if (input % 3 ==0)
+            if (input % 15 == 0)
+            { return "FizzBuzz"; }
+
+            if (input %5 == 0)
+            {
+                return "Buzz";
+            }
+            if (input % 3 == 0)
             { return "Fizz"; }
+
             return input.ToString();
         }
     }
